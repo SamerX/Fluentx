5779bb437dfbd14e57aa87b53cef77a056258590
Samer Aburabie <samer@aburabie.me>
2024-07-08 19:21:01 +0400

    Adding support for async in specifications.

--- Fluentx/Specifications.cs
+++ Fluentx/Specifications.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Fluentx
 {
     /// <summary>
-    /// Represents the core of the specificaiton pattern
+    /// Represents the core of the specification pattern
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public interface ISpecification<T>
@@ -23,11 +24,25 @@ namespace Fluentx
         Result<bool> Validate(T instance);
 
         /// <summary>
+        /// Validate the specification and return the boolean result along with any error messages in Result.ErrorMessages, might not all specification run.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        Task<Result<bool>> ValidateAsync(T instance);
+
+        /// <summary>
         /// Validate the specification and return the boolean result along with any error messages in Result.ErrorMessages, all specifications will run regardless of any shortcuts.
         /// </summary>
         /// <param name="instance"></param>
         /// <returns></returns>
         Result<bool> ValidateAll(T instance);
+
+        /// <summary>
+        /// Validate the specification and return the boolean result along with any error messages in Result.ErrorMessages, all specifications will run regardless of any shortcuts.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        Task<Result<bool>> ValidateAllAsync(T instance);
 
         //ISpecification<T> Not();
         /// <summary>
@@ -38,7 +53,7 @@ namespace Fluentx
         ISpecification<T> And(ISpecification<T> specification);
 
         /// <summary>
-        /// Or a specificaiton with another
+        /// Or a specification with another
         /// </summary>
         /// <param name="specification"></param>
         /// <returns></returns>
@@ -79,16 +94,23 @@ namespace Fluentx
     public abstract class CompositeSpecification<T> : ISpecification<T>
     {
         /// <summary>
-        /// Message returned for the specificaiton validation
+        /// Message returned for the specification validation
         /// </summary>
         public IEnumerable<string> Messages { get; set; }
 
         /// <summary>
-        /// Executes and Validates the specificaiton
+        /// Executes and Validates the specification
         /// </summary>
         /// <param name="instance"></param>
         /// <returns></returns>
         public abstract Result<bool> Validate(T instance);
+
+        /// <summary>
+        /// Executes and Validates the specification
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public abstract Task<Result<bool>> ValidateAsync(T instance);
 
         /// <summary>
         /// Validate the specification and returns success or failure.
@@ -96,6 +118,13 @@ namespace Fluentx
         /// <param name="instance"></param>
         /// <returns></returns>
         public abstract Result<bool> ValidateAll(T instance);
+
+        /// <summary>
+        /// Validate the specification and returns success or failure.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public abstract Task<Result<bool>> ValidateAllAsync(T instance);
 
         /// <summary>
         /// Creates a new Specification holding current specification AND specified specification
@@ -212,7 +241,7 @@ namespace Fluentx
         }
 
         /// <summary>
-        /// Executes and validates the specificaiton, this will NOT continue to the next specification if the validation fails
+        /// Executes and validates the specification, this will NOT continue to the next specification if the validation fails
         /// </summary>
         /// <param name="instance"></param>
         /// <returns></returns>
@@ -243,7 +272,38 @@ namespace Fluentx
         }
 
         /// <summary>
-        /// Executes and validates the specificaiton, this will continue to the next specification if the validation fails
+        /// Executes and validates the specification, this will NOT continue to the next specification if the validation fails
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override async Task<Result<bool>> ValidateAsync(T instance)
+        {
+            (this.Messages as IList<string>)?.Clear();
+
+            Result<bool> leftResult;
+
+            if ((leftResult = await this.leftSpecification.ValidateAsync(instance)).Data)
+            {
+                Result<bool> rightResult;
+                if ((rightResult = await this.rightSpecification.ValidateAsync(instance)).Data)
+                {
+                    return Result.Return(true);
+                }
+                else
+                {
+                    this.Messages = this.Messages.Concat(rightResult.ErrorMessages);
+                }
+            }
+            else
+            {
+                this.Messages = this.Messages.Concat(leftResult.ErrorMessages);
+            }
+
+            return Result.Return(false);
+        }
+
+        /// <summary>
+        /// Executes and validates the specification, this will continue to the next specification if the validation fails
         /// </summary>
         /// <param name="instance"></param>
         /// <returns></returns>
@@ -253,6 +313,38 @@ namespace Fluentx
 
             var leftResult = this.leftSpecification.ValidateAll(instance);
             var rightResult = this.rightSpecification.ValidateAll(instance);
+
+            if (!leftResult.Data)
+            {
+                this.Messages = this.Messages.Concat(leftResult.ErrorMessages);
+            }
+
+            if (!rightResult.Data)
+            {
+                this.Messages = this.Messages.Concat(rightResult.ErrorMessages);
+            }
+
+            var result = leftResult.Data & rightResult.Data;
+
+            if (result)
+            {
+                (this.Messages as IList<string>)?.Clear();
+            }
+
+            return Result.Return(result);
+        }
+
+        /// <summary>
+        /// Executes and validates the specification, this will continue to the next specification if the validation fails
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override async Task<Result<bool>> ValidateAllAsync(T instance)
+        {
+            (this.Messages as IList<string>)?.Clear();
+
+            var leftResult = await this.leftSpecification.ValidateAllAsync(instance);
+            var rightResult = await this.rightSpecification.ValidateAllAsync(instance);
 
             if (!leftResult.Data)
             {
@@ -330,6 +422,39 @@ namespace Fluentx
         }
 
         /// <summary>
+        /// Executes and validates the specification returning the boolean result, in case of failed validation it will stop on the first failed validation without executing the remaining ones in the chain of validations.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override async Task<Result<bool>> ValidateAsync(T instance)
+        {
+            (this.Messages as IList<string>)?.Clear();
+
+            Result<bool> leftResult;
+            if ((leftResult = await this.leftSpecification.ValidateAsync(instance)).Data)
+            {
+                return Result.Return(true);
+            }
+            else
+            {
+                this.Messages = this.Messages.Concat(leftResult.ErrorMessages).ToList();
+
+                Result<bool> rightResult;
+                if ((rightResult = await this.rightSpecification.ValidateAsync(instance)).Data)
+                {
+                    (this.Messages as IList<string>)?.Clear();
+                    return Result.Return(true);
+                }
+                else
+                {
+                    this.Messages = this.Messages.Concat(rightResult.ErrorMessages).ToList();
+                }
+            }
+
+            return Result.Return(false);
+        }
+
+        /// <summary>
         /// Executes and validates the specification, this will NOT continue to the next specification if the validation fails
         /// </summary>
         /// <param name="instance"></param>
@@ -340,6 +465,37 @@ namespace Fluentx
 
             var leftResult = this.leftSpecification.ValidateAll(instance);
             var rightResult = this.rightSpecification.ValidateAll(instance);
+
+            if (!leftResult.Data)
+            {
+                this.Messages = this.Messages.Concat(leftResult.ErrorMessages).ToList();
+            }
+
+            if (!rightResult.Data)
+            {
+                this.Messages = this.Messages.Concat(rightResult.ErrorMessages).ToList();
+            }
+
+            var result = leftResult.Data | rightResult.Data;
+            if (result)
+            {
+                (this.Messages as IList<string>)?.Clear();
+            }
+
+            return Result.Return(result);
+        }
+
+        /// <summary>
+        /// Executes and validates the specification, this will NOT continue to the next specification if the validation fails
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override async Task<Result<bool>> ValidateAllAsync(T instance)
+        {
+            (this.Messages as IList<string>)?.Clear();
+
+            var leftResult = await this.leftSpecification.ValidateAllAsync(instance);
+            var rightResult = await this.rightSpecification.ValidateAllAsync(instance);
 
             if (!leftResult.Data)
             {
@@ -406,6 +562,29 @@ namespace Fluentx
         }
 
         /// <summary>
+        /// Executes and validates the specification
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override async Task<Result<bool>> ValidateAsync(T instance)
+        {
+            (this.Messages as IList<string>)?.Clear();
+
+            var leftResult = await this.leftSpecification.ValidateAsync(instance);
+            var rightResult = await this.rightSpecification.ValidateAsync(instance);
+
+            var xorResult = leftResult.Data ^ rightResult.Data;
+
+            if (!xorResult)
+            {
+                this.Messages = this.Messages.Concat(leftResult.ErrorMessages);
+                this.Messages = this.Messages.Concat(rightResult.ErrorMessages);
+            }
+
+            return Result.Return(xorResult);
+        }
+
+        /// <summary>
         /// Executes and validates the specification (for Xor its no difference with Validate Method)
         /// </summary>
         /// <param name="instance"></param>
@@ -427,64 +606,28 @@ namespace Fluentx
 
             return Result.Return(xorResult);
         }
-    }
-
-    /// <summary>
-    /// Represents an expression based specification
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class ExpressionSpecification<T> : CompositeSpecification<T>
-    {
-        private readonly Func<T, bool> expression;
 
         /// <summary>
-        /// Creates an expression specification
-        /// </summary>
-        /// <param name="expression"></param>
-        /// <param name="messages"></param>
-        public ExpressionSpecification(Func<T, bool> expression, IEnumerable<string> messages)
-        {
-            if (expression == null)
-                throw new ArgumentNullException();
-            else
-                this.expression = expression;
-            this.Messages = messages;
-        }
-
-        /// <summary>
-        /// Creates an expression specification
-        /// </summary>
-        /// <param name="expression"></param>
-        /// <param name="message"></param>
-        public ExpressionSpecification(Func<T, bool> expression, string message)
-        {
-            if (expression == null)
-                throw new ArgumentNullException();
-            else
-                this.expression = expression;
-            this.Messages = new[] { message };
-        }
-
-        /// <summary>
-        /// Validate the specification and return true or false
+        /// Executes and validates the specification (for Xor its no difference with Validate Method)
         /// </summary>
         /// <param name="instance"></param>
         /// <returns></returns>
-        public override Result<bool> Validate(T instance)
+        public override async Task<Result<bool>> ValidateAllAsync(T instance)
         {
-            var result = this.expression(instance);
-            return Result.Return(result, this.Messages);
-        }
+            (this.Messages as IList<string>)?.Clear();
 
-        /// <summary>
-        /// Will validate the specification and continue to the next node in rules chain.
-        /// </summary>
-        /// <param name="instance"></param>
-        /// <returns></returns>
-        public override Result<bool> ValidateAll(T instance)
-        {
-            var result = this.expression(instance);
-            return Result.Return(result, this.Messages);
+            var leftResult = await this.leftSpecification.ValidateAllAsync(instance);
+            var rightResult = await this.rightSpecification.ValidateAllAsync(instance);
+
+            var xorResult = leftResult.Data ^ rightResult.Data;
+
+            if (!xorResult)
+            {
+                this.Messages = this.Messages.Concat(leftResult.ErrorMessages);
+                this.Messages = this.Messages.Concat(rightResult.ErrorMessages);
+            }
+
+            return Result.Return(xorResult);
         }
     }
 
@@ -532,6 +675,33 @@ namespace Fluentx
             return result;
         }
 
+
+        /// <summary>
+        /// Executes and validate the specification
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override async Task<Result<bool>> ValidateAsync(T instance)
+        {
+            (this.Messages as IList<string>)?.Clear();
+            var result = await this.specification.ValidateAsync(instance);
+
+            if (!result.Data)
+            {
+                //Validation is passed (as its a negate)
+                return Result.Return(true);
+            }
+            else
+            {
+                foreach (var errorMessage in result.ErrorMessages)
+                {
+                    (this.Messages as IList<string>)?.Add("NOT:" + errorMessage);
+                }
+            }
+
+            return result;
+        }
+
         /// <summary>
         /// Validates and continues
         /// </summary>
@@ -556,6 +726,142 @@ namespace Fluentx
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Validates and continues
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override async Task<Result<bool>> ValidateAllAsync(T instance)
+        {
+            (this.Messages as IList<string>)?.Clear();
+            var result = await this.specification.ValidateAllAsync(instance);
+
+            if (!result.Data)
+            {
+                //Validation is passed (as its a negate)
+                return Result.Return(true);
+            }
+            else
+            {
+                foreach (var errorMessage in result.ErrorMessages)
+                {
+                    (this.Messages as IList<string>)?.Add("NOT:" + errorMessage);
+                }
+            }
+
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// Represents an expression based specification
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ExpressionSpecification<T> : CompositeSpecification<T>
+    {
+        private readonly Func<T, bool> expression;
+        private readonly Func<T, Task<bool>> asyncExpression;
+
+        /// <summary>
+        /// Creates an expression specification
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="messages"></param>
+        public ExpressionSpecification(Func<T, bool> expression, IEnumerable<string> messages)
+        {
+            if (expression == null)
+                throw new ArgumentNullException();
+            else
+                this.expression = expression;
+            this.Messages = messages;
+        }
+
+        /// <summary>
+        /// Creates an expression specification
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="messages"></param>
+        public ExpressionSpecification(Func<T, Task<bool>> expression, IEnumerable<string> messages)
+        {
+            if (expression == null)
+                throw new ArgumentNullException();
+            else
+                this.asyncExpression = expression;
+            this.Messages = messages;
+        }
+
+        /// <summary>
+        /// Creates an expression specification
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="message"></param>
+        public ExpressionSpecification(Func<T, bool> expression, string message)
+        {
+            if (expression == null)
+                throw new ArgumentNullException();
+            else
+                this.expression = expression;
+            this.Messages = new[] { message };
+        }
+
+        /// <summary>
+        /// Creates an expression specification
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="message"></param>
+        public ExpressionSpecification(Func<T, Task<bool>> expression, string message)
+        {
+            if (expression == null)
+                throw new ArgumentNullException();
+            else
+                this.asyncExpression = expression;
+            this.Messages = new[] { message };
+        }
+
+        /// <summary>
+        /// Validate the specification and return true or false
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override Result<bool> Validate(T instance)
+        {
+            var result = this.expression?.Invoke(instance) ?? this.asyncExpression(instance).Result;
+            return Result.Return(result, this.Messages);
+        }
+
+        /// <summary>
+        /// Validate the specification and return true or false
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override async Task<Result<bool>> ValidateAsync(T instance)
+        {
+            var result = this.asyncExpression != null
+                ? await this.asyncExpression(instance)
+                : this.expression(instance);
+            return Result.Return(result, this.Messages);
+        }
+
+        /// <summary>
+        /// Will validate the specification and continue to the next node in rules chain.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override Result<bool> ValidateAll(T instance)
+        {
+            return Validate(instance);
+        }
+
+        /// <summary>
+        /// Will validate the specification and continue to the next node in rules chain.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public override async Task<Result<bool>> ValidateAllAsync(T instance)
+        {
+            return await ValidateAsync(instance);
         }
     }
 }
