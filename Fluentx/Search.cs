173d4929c68030502ee6a77778d0eb48a070628f
Samer Aburabie <samer@aburabie.me>
2024-07-08 12:29:43 +0400

    Adding Search and Search<T> classes

---
+++ Fluentx/Search.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluentx
+{
+    /// <summary>
+    /// A simple container class to transfer search related data.
+    /// </summary>
+    public class Search
+    {
+        /// <summary>
+        /// Page Index
+        /// </summary>
+        public int PageIndex { get; set; }
+
+        /// <summary>
+        /// Page Size
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// Sort By
+        /// </summary>
+        public string SortBy { get; set; }
+
+        /// <summary>
+        /// Sort Direction
+        /// </summary>
+        public string SortDirection { get; set; }
+    }
+
+    /// <summary>
+    /// A simple container class to transfer search related data.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Search<T> : Search
+    {
+        /// <summary>
+        /// The criteria used for filter
+        /// </summary>
+        public T Criteria { get; set; }
+    }
+}
--- Fluentx/UISearch.cs
+++ Fluentx/UISearch.cs
@@ -7,27 +7,37 @@ using System.Threading.Tasks;
 namespace Fluentx
 {
     /// <summary>
-    /// A simple container class to transfer ui search related data.
+    /// A simple container class to transfer ui search related data. This class is the same as Search class, preferably to use Search class instead.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class UISearch<T>
+    public class UISearch
     {
         /// <summary>
         /// Page Index
         /// </summary>
         public int PageIndex { get; set; }
+
         /// <summary>
         /// Page Size
         /// </summary>
         public int PageSize { get; set; }
+
         /// <summary>
         /// Sort By
         /// </summary>
         public string SortBy { get; set; }
+
         /// <summary>
         /// Sort Direction
         /// </summary>
         public string SortDirection { get; set; }
+    }
+
+    /// <summary>
+    /// A simple container class to transfer ui search related data. This class is the same as Search class, preferably to use Search class instead.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class UISearch<T> : UISearch
+    {
         /// <summary>
         /// The criteria used for filter
         /// </summary>
